7cf17f2e1e460482e0f5befa93b59d5e7237987f
sspatari <spatari.stanislav@gmail.com>
2017-05-15 22:49:48 +0300

    add index.js, the main file to start server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+      app = express(),
+      config = require('./config'),
+      mongoose = require('mongoose'),
+      bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+mongoose.connect(config.database, (err) => {
+  console.log('Successfuly connected to ' + config.database)
+})
+mongoose.Promise = global.Promise
+
+app.get('/', (req, res) => {
+  res.send({message: 'Hi'})
+})
+
+app.listen(config.port, () => {
+  console.log('Application is running on port ' + config.port)
+})
