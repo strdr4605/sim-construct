4f99cfe48ab6daf849d23adc4e2707afd39dac43
Sasha <s.sasha44@yahoo.com>
2017-05-24 15:46:10 +0300

    add bootstrap-vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 import './assets/css/base/style.css'
@@ -8,8 +9,11 @@ import './assets/css/layout/layout.css'
 import './assets/css/modules/header.css'
 import './assets/css/modules/content.css'
 import './assets/css/modules/sidebar.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
