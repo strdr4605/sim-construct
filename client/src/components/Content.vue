f5ff2dcb7041e9100ed7aea7db1ab66359af2ee8
strdr4605 <str.dr4605@gmail.com>
2017-06-01 13:21:54 +0300

    Add Content component

---
+++ client/src/components/Content.vue
@@ -0,0 +1,170 @@
+<template lang="html">
+  <div id="content">
+    <div class="products">
+      <ul>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="product">
+            <div class="product-image">
+              <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
+            </div>
+            <div class="product-info">
+              <div class="product-price">
+                Clește 65 lei
+              </div>
+              <div class="product-description">
+                <ul>
+                  <li>Tip: clește</li>
+                  <li>Burete Material: Oțel</li>
+                  <li>Mâner Material: plastic</li>
+                  <li>Scop: să taie din metal, plastic</li>
+                  <li>Lungime: 180 mm</li>
+                  <li>Tara de origine: Polonia</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,175 +1,18 @@
 <template lang="html">
   <div id="main" class="container">
         <my-sidebar></my-sidebar>
-        <div id="content">
-          <div class="products">
-            <ul>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="product">
-                  <div class="product-image">
-                    <span class="helper"></span><img src="assets/dist/images/cleste.png" alt="">
-                  </div>
-                  <div class="product-info">
-                    <div class="product-price">
-                      Clește 65 lei
-                    </div>
-                    <div class="product-description">
-                      <ul>
-                        <li>Tip: clește</li>
-                        <li>Burete Material: Oțel</li>
-                        <li>Mâner Material: plastic</li>
-                        <li>Scop: să taie din metal, plastic</li>
-                        <li>Lungime: 180 mm</li>
-                        <li>Tara de origine: Polonia</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <my-content></my-content>
       </div>
 </template>
 
 <script>
 import MySidebar from './Sidebar'
+import MyContent from './Content'
 
 export default {
   components: {
-    MySidebar
+    MySidebar,
+    MyContent
   }
 }
 </script>
